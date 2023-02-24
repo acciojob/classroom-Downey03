63bb472c74a74ad2da9d54e16d31e7385a3fff6c
Karthick Raj K <karthickraj2001@gmail.com>
2023-02-24 11:23:22 +0530

    Tasks completed

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -9,4 +9,42 @@ public class Student {
     public Student(){
 
     }
+
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public boolean compareTo(Student s2){
+        if(this.getAge() != s2. getAge()) return false;
+        if(!this.getName().equals(s2.getName())) return false;
+        if(this.getAverageScore() != s2.getAverageScore()) return false;
+        return true;
+    }
+
 }
